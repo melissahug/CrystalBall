d0f097bc4f15812cd7d4f039357b456b4476cb7c
Melissa <hughes.melissa1@gmail.com>
2016-10-25 22:54:25 -0400

    first commit

---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        //get random number
+        //create an array of strings
+        public string getResponse()
+        {
+            string[] responses = { "Don't be a jerk.", "Life is short. Play hard.", "Go Cleveland. Whatever team." };
+            //get random object
+            Random randomNum = new Random();
+            int num = randomNum.Next(0, responses.Length);
+            return responses[num];
+
+        }
+    }
+}
